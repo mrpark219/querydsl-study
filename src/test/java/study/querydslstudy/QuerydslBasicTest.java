00ef49da15eba5c70821d3da24d34652c0e662dc
mrpark219 <mrpark219@gmail.com>
2024-03-07 02:29:30 +0900

    ✨ case 문

--- src/test/java/study/querydslstudy/QuerydslBasicTest.java
+++ src/test/java/study/querydslstudy/QuerydslBasicTest.java
@@ -2,6 +2,7 @@ package study.querydslstudy;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
@@ -460,6 +461,44 @@ public class QuerydslBasicTest {
 		// then
 		for(Tuple tuple : result) {
 			System.out.println("Tuple: " + tuple);
+		}
+	}
+
+	@DisplayName("case 문 - 기본")
+	@Test
+	void basicCase() {
+
+		// when
+		List<String> result = queryFactory
+			.select(member.age
+				.when(10).then("열살")
+				.when(20).then("스무살")
+				.otherwise("기타"))
+			.from(member)
+			.fetch();
+
+		// then
+		for(String s : result) {
+			System.out.println("s: " + s);
+		}
+	}
+
+	@DisplayName("case 문 - 심화")
+	@Test
+	void complexCase() {
+
+		// when
+		List<String> result = queryFactory
+			.select(new CaseBuilder()
+				.when(member.age.between(0, 20)).then("0~20살")
+				.when(member.age.between(21, 30)).then("21~30살")
+				.otherwise("기타"))
+			.from(member)
+			.fetch();
+
+		// then
+		for(String s : result) {
+			System.out.println("s: " + s);
 		}
 	}
 }
