df29fdf5cbb0c4aa8b6dbcb93a15ecb7f4bb769f
mrpark219 <mrpark219@gmail.com>
2024-03-07 01:55:57 +0900

    ✨ 조인 - on 절

--- src/test/java/study/querydslstudy/QuerydslBasicTest.java
+++ src/test/java/study/querydslstudy/QuerydslBasicTest.java
@@ -292,4 +292,45 @@ public class QuerydslBasicTest {
 			.extracting("username")
 			.containsExactly("teamA", "teamB");
 	}
+
+	@DisplayName("회원과 팀을 조인하면서, 팀 이름이 teamA인 팀만 조인, 회원은 모두 조회")
+	@Test
+	void join_on_filtering() {
+
+		// when
+		List<Tuple> result = queryFactory
+			.select(member, team)
+			.from(member)
+			.leftJoin(member.team, team)
+			.on(team.name.eq("teamA"))
+			.fetch();
+
+		// then
+		for(Tuple tuple : result) {
+			System.out.println("Tuple: " + tuple);
+		}
+	}
+	
+	@DisplayName("세타 조인 - 회원의 이름이 팀 이름과 같은 회원 조회")
+	@Test
+	void join_on_no_relation() {
+
+		// given
+		em.persist(new Member(("teamA")));
+		em.persist(new Member(("teamB")));
+		em.persist(new Member(("teamC")));
+
+		// when
+		List<Tuple> result = queryFactory
+			.select(member, team)
+			.from(member)
+			.leftJoin(team)
+			.on(member.username.eq(team.name))
+			.fetch();
+
+		// then
+		for(Tuple tuple : result) {
+			System.out.println("Tuple: " + tuple);
+		}
+	}
 }
