3d6a362624030548ed9f3d9d1b68f9860cf0cde2
mrpark219 <mrpark219@gmail.com>
2024-03-09 22:54:59 +0900

    ✨ 인터페이스 지원 - QuerydslPredicateExecutor

--- src/test/java/study/querydslstudy/repository/MemberRepositoryTest.java
+++ src/test/java/study/querydslstudy/repository/MemberRepositoryTest.java
@@ -16,6 +16,7 @@ import study.querydslstudy.entity.Team;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static study.querydslstudy.entity.QMember.member;
 
 @SpringBootTest
 @Transactional
@@ -107,5 +108,37 @@ class MemberRepositoryTest {
 		// then
 		assertThat(result.getSize()).isEqualTo(3);
 		assertThat(result.getContent()).extracting("username").containsExactly("member1", "member2", "member3");
+	}
+
+	@DisplayName("querydslPredicateExecutor")
+	@Test
+	void querydslPredicateExecutor() {
+
+		// given
+		Team teamA = new Team("teamA");
+		Team teamB = new Team("teamB");
+		em.persist(teamA);
+		em.persist(teamB);
+
+		Member member1 = new Member("member1", 10, teamA);
+		Member member2 = new Member("member2", 20, teamA);
+
+		Member member3 = new Member("member3", 30, teamB);
+		Member member4 = new Member("member4", 40, teamB);
+		em.persist(member1);
+		em.persist(member2);
+		em.persist(member3);
+		em.persist(member4);
+
+	    // when
+		Iterable<Member> result = memberRepository.findAll(
+			member.age.between(10, 40)
+				.and(member.username.eq("member1"))
+		);
+
+		// then
+		for(Member member : result) {
+			System.out.println("member = " + member);
+		}
 	}
 }
